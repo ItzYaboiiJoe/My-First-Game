c41f80aea162c3cedda16e28f3aca71a4918b107
ItzYaboiiJoe <josephakseoudy@gmail.com>
2024-03-25 18:57:58 -0400

    Updated Movements Again

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,11 +24,6 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButton("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
         Flip();
     }
 
